e93c365efcbbe74200ae12e098fd47ab57877436
keymatic <23658423+keymatic@users.noreply.github.com>
2023-07-26 23:56:58 +0400

    Added DbContext

---
+++ Key.AiAssistant.Store/AiAssistantDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Key.AiAssistant.Domain;
+using Key.AiAssistant.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Key.AiAssistant.Store
+{
+    public class AiAssistantDbContext : DbContext
+    {
+        public AiAssistantDbContext(DbContextOptions<AiAssistantDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AiAssistantDbContext).Assembly);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                entry.Entity.UpdatedDate = DateTimeOffset.UtcNow;
+
+                if (entry.State == EntityState.Added)
+                    entry.Entity.CreatedDate = DateTimeOffset.UtcNow;
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<Prompt> Prompts { get; set; }
+        public DbSet<Conversation> Conversations { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
